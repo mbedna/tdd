44282409cfb89149694fed5e81a8320579df1df2
paws77 <paws77@gmail.com>
2012-10-23 21:37:45 +0200

    Extracted interface for factory

--- src/main/java/com/om/example/BinaryOperatorFactory.java
+++ src/main/java/com/om/example/BinaryOperatorFactory.java
@@ -3,17 +3,23 @@ package com.om.example;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BinaryOperatorFactory {
+public class BinaryOperatorFactory implements OperatorFactory {
 
-	private Map<String, MathOperator> operators = new HashMap<String, MathOperator>(); 
+	private Map<String, MathOperator> operators;
 
 	public BinaryOperatorFactory() {
+		this.operators = new HashMap<String, MathOperator>(); 
+		this.registerOperators();
+	}
+			
+	private void registerOperators() {
 		this.operators.put("+", new Add());
 		this.operators.put("-", new Substract());
 		this.operators.put("*", new Multiply());
 		this.operators.put("/", new Divide());
 		this.operators.put("!", new Factorial());
 	}
+
 	public MathOperator create(String operatorName) {
 		MathOperator op = this.operators.get(operatorName);
 		if (op == null) {
