3ed23058778d1927942b68907989c81b562f5c05
Marcin Bednarek <paws77@gmail.com>
2012-10-31 14:24:37 +0100

    Use factory.

---
+++ src/main/java/com/om/example/CompositeMathOperator.java
@@ -0,0 +1,26 @@
+package com.om.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CompositeMathOperator implements MathOperator {
+	
+	List<MathOperator> mathOperators;
+	private OperatorFactory operatorFactory;
+
+	public CompositeMathOperator() {
+		this.mathOperators = new ArrayList<MathOperator>();
+		this.operatorFactory = new BinaryOperatorFactoryImpl();
+	}
+
+	@Override
+	public void execute(OperandStack operandStack) {
+		for(int i = 0; i < mathOperators.size(); i++) {
+			mathOperators.get(i).execute(operandStack);
+		}	
+	}
+	
+	public void register(String op) {
+		this.mathOperators.add(operatorFactory.create(op));
+	}
+}
---
+++ src/test/java/com/om/example/CompositeMathOperatorTest.java
@@ -0,0 +1,52 @@
+package com.om.example;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompositeMathOperatorTest {
+	private CompositeMathOperator compositeMathOperator;
+	private OperatorFactory factory;
+	private OperandStack operandStack;
+
+	@Before
+	public void init() {
+		compositeMathOperator = new CompositeMathOperator();
+		factory = new BinaryOperatorFactoryImpl();
+		operandStack = new OperandStack();
+	}
+
+	@Test
+	public void shouldBeAbleToRegisterManyOperators() {
+		compositeMathOperator.register("+");
+		compositeMathOperator.register("*");
+		compositeMathOperator.execute(operandStack);
+		assertThat(compositeMathOperator.mathOperators.size(), equalTo(2));
+	}
+
+	@Test
+	public void methodExecuteForEachOperatorShouldBeInvoked() {
+		operandStack.push(new BigDecimal(2));
+		operandStack.push(new BigDecimal(3));
+		operandStack.push(new BigDecimal(4));
+		compositeMathOperator.register("+");
+		compositeMathOperator.register("*");
+		compositeMathOperator.execute(operandStack);
+		assertThat(operandStack.peek(), equalTo(new BigDecimal(14)));
+	}
+
+	@Test
+	public void divisionAndSubstart() {
+		operandStack.push(new BigDecimal(5));
+		operandStack.push(new BigDecimal(8));
+		operandStack.push(new BigDecimal(4));
+		compositeMathOperator.register("/");
+		compositeMathOperator.register("-");
+		compositeMathOperator.execute(operandStack);
+		assertThat(operandStack.peek(), equalTo(new BigDecimal(3)));
+	}
+}
