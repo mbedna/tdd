a1f99d9c0a2c77e5c293dcc2c88df3190c6e9066
Marcin Bednarek <paws77@gmail.com>
2012-10-31 12:48:45 +0100

    Using BDD to created PrimeFactors method.

--- src/main/java/com/om/example/PrimeFactorsII.java
+++ src/main/java/com/om/example/PrimeFactorsII.java
@@ -3,17 +3,27 @@ package com.om.example;
 import java.math.BigDecimal;
 
 public class PrimeFactorsII implements MathOperator {
-	
+
 	private BigDecimal TWO = new BigDecimal(2);
 
 	@Override
 	public void execute(OperandStack operandStack) {
-		BigDecimal value = operandStack.peek();	
-		operandStack.pop();		
-		if (value.compareTo(TWO) == 1 && value.remainder(TWO).equals(BigDecimal.ZERO)) {
-			operandStack.push(TWO);
-			value = TWO;
+		BigDecimal value = operandStack.peek();
+		operandStack.pop();
+
+		if (value.compareTo(BigDecimal.ONE) == 0) {
+			operandStack.push(value);
 		}
-		operandStack.push(value);
+
+		BigDecimal divisor = TWO;
+		while (value.compareTo(divisor) >= 0) {
+			while (value.remainder(divisor).equals(BigDecimal.ZERO)) {
+				operandStack.push(divisor);
+				value = value.divide(divisor);
+			}
+			divisor = divisor.add(BigDecimal.ONE);
+		}
+
 	}
+
 }
--- src/test/java/com/om/example/PrimeFactorsIITest.java
+++ src/test/java/com/om/example/PrimeFactorsIITest.java
@@ -1,11 +1,12 @@
 package com.om.example;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
 
 public class PrimeFactorsIITest {
 
@@ -52,13 +53,41 @@ public class PrimeFactorsIITest {
 		and(new BigDecimal(2));
 	}
 
-	// @Test
+	@Test
 	public void of6ShouldReturnDouble2() {
-		givenValue(new BigDecimal(4));
+		givenValue(new BigDecimal(6));
+		whenPrimeFactorsIIIsInvoked();
+		thenValueOnStackShouldBe(new BigDecimal(3));
+		and(new BigDecimal(2));
+	}
+
+	@Test
+	public void of8ShouldReturThreeTimes2() {
+		givenValue(new BigDecimal(8));
 		whenPrimeFactorsIIIsInvoked();
 		thenValueOnStackShouldBe(new BigDecimal(2));
 		and(new BigDecimal(2));
+		and(new BigDecimal(2));
 	}
+
+	@Test
+	public void of9ShouldReturDouble3() {
+		givenValue(new BigDecimal(9));
+		whenPrimeFactorsIIIsInvoked();
+		thenValueOnStackShouldBe(new BigDecimal(3));
+		and(new BigDecimal(3));
+	}
+	
+	@Test
+	public void of1024ShouldReturTen2() {
+		givenValue(new BigDecimal(1024));
+		whenPrimeFactorsIIIsInvoked();
+		thenValueOnStackShouldBe(new BigDecimal(2));
+		for (int i = 0; i < 9; i++) {
+			and(new BigDecimal(2));
+		}
+	}
+
 
 	private void givenValue(BigDecimal value) {
 		operandStack.push(value);
@@ -71,6 +100,7 @@ public class PrimeFactorsIITest {
 
 	private void thenValueOnStackShouldBe(BigDecimal value) {
 		assertThat(operandStack.peek(), equalTo(value));
+		operandStack.pop();
 	}
 
 	private void and(BigDecimal value) {
